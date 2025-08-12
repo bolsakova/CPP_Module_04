d76c16f4d275cdae5acb8d744dbe3a5f41de2079
bolsakova <bolsakova@github.com>
2025-08-12 23:19:34 +0200

    ex00: created header for Cat class

---
+++ ex00/Cat.hpp
@@ -0,0 +1,14 @@
+#ifndef CAT_HPP
+# define CAT_HPP
+
+#include <iostream>
+#include <string>
+#include "Animal.hpp"
+
+class Cat : public Animal {
+	
+	public:
+			void makeSound() override;
+};
+
+#endif
