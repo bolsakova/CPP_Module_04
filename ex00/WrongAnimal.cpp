b5c1134658827976a1644498e60df078be2734cb
bolsakova <bolsakova@github.com>
2025-08-13 12:00:19 +0200

    ex00: WrongAnimal class realization finished

---
+++ ex00/WrongAnimal.cpp
@@ -0,0 +1,33 @@
+#include "WrongAnimal.hpp"
+
+WrongAnimal::WrongAnimal() : type("WrongAnimal") {
+	std::cout << "WrongAnimal default constructor called" << std::endl;
+}
+
+WrongAnimal::WrongAnimal(const std::string& type) : type(type) {
+	std::cout << "WrongAnimal parameterized constructor called" << std::endl;
+}
+
+WrongAnimal::WrongAnimal(const WrongAnimal& other) : type(other.type) {
+	std::cout << "WrongAnimal copy constructor called" << std::endl;
+}
+
+WrongAnimal& WrongAnimal::operator=(const WrongAnimal& other) {
+	std::cout << "WrongAnimal assignment operator called" << std::endl;
+	if (this != &other)
+		type = other.type;
+	return *this;
+}
+
+WrongAnimal::~WrongAnimal() {
+	std::cout << "WrongAnimal destructor called" << std::endl;
+}
+
+void WrongAnimal::makeSound() {
+	std::cout << "WrongAnimal makes a weird sound" << std::endl;
+}
+
+const std::string& WrongAnimal::getType() const {
+	return type;
+}
+
